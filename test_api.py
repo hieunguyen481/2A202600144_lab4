685fb29b0dfc16aec6ecf5d0cc1e0fa91b331058
hieunguyen481 <chihieu3603@gmail.com>
2026-04-07 15:15:53 +0700

    first commit

---
+++ test_api.py
@@ -0,0 +1,12 @@
+﻿import os
+from dotenv import load_dotenv
+from langchain_openai import ChatOpenAI
+
+load_dotenv()
+
+# api_key = os.getenv("OPENAI_API_KEY")
+
+llm = ChatOpenAI(model="gpt-4o-mini",
+                 api_key=os.getenv("OPENAI_API_KEY"))
+response = llm.invoke("Xin chao?")
+print(response.content)
